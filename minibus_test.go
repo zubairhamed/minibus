15782d00bea3ddbe4dd33e42cf06f94458ef2c96
Zubair Hamed <zubairhamed@gmail.com>
2015-07-09 19:54:02 +0800

    Test updates

--- minibus_test.go
+++ minibus_test.go
@@ -18,13 +18,21 @@ func TestMiniBus(t *testing.T) {
 	bus.Sub("/topic/second", func(interface{}){})
 	assert.Equal(t, 1, len(bus.GetSub("/topic/second")))
 
-
 	called := false
-	bus.Sub("/test/call", func(msg interface{}){
+	bus.Sub("/topic/called", func(msg interface{}){
 		called = true
 	})
-	bus.Pub("/test/call", "")
+	bus.Pub("/topic/called", "")
 	time.Sleep(1 * time.Second)
 
+	assert.Equal(t, 1, len(bus.GetSub("/topic/called")))
 	assert.True(t, called)
+
+	bus.ClearTopic("/topic/called")
+	assert.Equal(t, 0, len(bus.GetSub("/topic/called")))
+
+	assert.Equal(t, 2, len(bus.GetSub("/topic")))
+
+	bus.ClearSubs()
+	assert.Equal(t, 0, len(bus.GetSub("/topic")))
 }
